a5472b1bf3bcbad1a399d5fec75bc6191a81c8ad
Sathish DK <sathishdk@sathishdk.com>
2015-06-12 14:32:47 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  console.log("Hello");
+  
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  var row8 = [a8, b8, c8, d8, e8, f8, g8, h8];
+  
+  function addRow8(){
+    
+  }
+  
 });
