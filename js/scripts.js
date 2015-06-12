a0874454a80628452803db49ebf91f3a9affce17
Sathish DK <sathishdk@sathishdk.com>
2015-06-12 15:10:52 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-  var x = [ "a8", "b8", "c8" ];
+  var x = [ 
+            'a': { "a8", "b8", "c8" },
+            'b': { "a7", "b7", "c7" },
+          ];
   $.each( x, function( index, value ) {
     console.log( "value", value );
   });
