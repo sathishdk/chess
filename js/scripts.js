32228c1b695f02bd59b0538c169d9269bede5df6
Sathish DK <sathishdk@sathishdk.com>
2015-06-15 16:56:55 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  document.getElementById("showRules").addEventListener(click, function(){
+  document.getElementById("showRules").addEventListener('click', function(){
     document.getElementById("rulesData").style.display = "block";
   });
-  document.getElementById("rulesClose").addEventListener(click, function(){
+  document.getElementById("rulesClose").addEventListener('click', function(){
     document.getElementById("rulesData").style.display = "none";
   });
 });
